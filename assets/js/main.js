dd69ea23e3eec5e75bb50c7656aab45e00fa7a12
DIPANKAR <you@example.com>
2020-10-02 23:18:57 +0600

    done

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+const drop_btn = document.querySelector('.drop_btn');
+const menu_wrapper = document.querySelector('.wrapper');
+const menu_bar = document.querySelector('.menu-bar');
+const setting_drop = document.querySelector('.setting-drop');
+const help_drop = document.querySelector('.help-drop');
+const setting_item = document.querySelector('.setting-item');
+const help_item = document.querySelector('.help-item');
+const back_setting_btn = document.querySelector('.back-setting-btn');
+const back_help_btn = document.querySelector('.back-help-btn');
+
+
+
+drop_btn.addEventListener('click', function(){
+    menu_wrapper.classList.toggle('show');
+});
+
+// drop_btn.onclick = (()=>{
+    
+// });
+setting_item.addEventListener('click', function(){
+    menu_bar.style.marginLeft =  '-400px';
+    setTimeout (() =>{
+        setting_drop.style.display = 'block'
+    }, 100);
+});
+
+help_item.addEventListener('click', function(){
+    menu_bar.style.marginLeft =  '-400px';
+    setTimeout(()=>{
+        help_drop.style.display = 'block'
+    }, 100);
+    
+});
+back_setting_btn.addEventListener('click', function(){
+    menu_bar.style.marginLeft =  '0';
+    setTimeout(() =>{
+        setting_drop.style.display = 'none'
+    }, 100);
+});
+
+back_help_btn.addEventListener('click', function(){
+    menu_bar.style.marginLeft =  '0';
+    setTimeout(()=> {
+        help_drop.style.display = 'none'
+    }, 100);
+});
